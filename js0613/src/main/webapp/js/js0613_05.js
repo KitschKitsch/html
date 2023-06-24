f1d4c74594c30d639ca23cf21276dc02b31ddead
KitschKitsch <jss5794@naver.com>
2023-06-24 11:30:42 +0900

    0613

--- js0613/src/main/webapp/js/js0613_05.js
+++ js0613/src/main/webapp/js/js0613_05.js
@@ -6,20 +6,22 @@ var interval;
 
 //setInterval 추가
 function setIn(){
-	var htmlData = "";
-	htmlData += "<tr>";
-	htmlData += "<td>";
-	htmlData += "1";
-	htmlData += "</td>";
-	htmlData += "</tr>";
-
-	//var data =  document.getElementById("t01").innerHTML = htmlData;
-	//var data =  document.getElementById("t01").innerText = htmlData;
-	$("#t01").append(htmlData);// 제이쿼리문 $("아이디")
 
 	interval = setInterval(function(){// interval 함수 생성
 		count += 1;
 		console.log("숫자 : ",count);
+		
+		var htmlData = "";
+		htmlData += "<tr>";
+		htmlData += "<td>";
+		htmlData += count;
+		htmlData += "</td>";
+		htmlData += "</tr>";
+	
+		//var data =  document.getElementById("t01").innerHTML = htmlData;
+		//var data =  document.getElementById("t01").innerText = htmlData;
+		$("#t01").append(htmlData);// 제이쿼리문 $("아이디")
+		
 	},1000); //   1/1000초 1000=1초
 }
 
